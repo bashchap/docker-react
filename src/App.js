72244797ebef9de5e46c0b72ddae81fbe63c8a4b
Tara Ram <taralalram@gmail.com>
2022-05-29 14:02:48 +0100

    I did not undo the changes properly

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed!
+          Execute order 69!
         </p>
         <a
           className="App-link"
